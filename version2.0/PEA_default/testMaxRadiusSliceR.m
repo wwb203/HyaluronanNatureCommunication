8b2bad037c1ff374247fa10af842438af16282f5
Wenbin Wei <wenbin.wei@varian.com>
2018-12-21 21:11:19 -0500

    clean up

--- version2.0/PEA_default/testMaxRadiusSliceR.m
+++
@@ -1,7 +0,0 @@
-filename = 'Image0013.oif';
-data = bfopen(filename);
-debug = false;
-[maxRadiusIdR,rArrayR]=maxRadiusSliceR(data,debug);
-%[maxRadiusIdG,rArrayG]=maxRadiusSlice(data,debug)
-figure(12)
-plot(1:length(rArrayR),rArrayR,'-r',1:length(rArrayG),rArrayG,'g*')
